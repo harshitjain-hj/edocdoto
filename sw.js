0c6dfdbd1d07184258273633097e2bd806a0d5be
edoc99 <harshitjainf3@gmail.com>
2020-08-03 11:12:13 +0530

    cache the important pages

--- sw.js
+++ sw.js
@@ -1,12 +1,51 @@
+const staticCacheName = 'site-static-v1';
+const assets = [
+  '/',
+  'index.html',
+  'js/app.js',
+  'js/ui.js',
+  'js/materialize.min.js',
+  'css/styles.css',
+  'css/materialize.min.css',
+  'img/dish.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v54/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+];
+
 // install service worker
 self.addEventListener('install', evt => {
-    console.log("service worker has been installed!");
-});
+    //console.log('service worker installed');
+    evt.waitUntil(
+        // caching assets
+        caches.open(staticCacheName).then((cache) => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
+  });
+
 // activate service worker
 self.addEventListener('activate', evt => {
-    console.log("service worker has been activated");
+    // console.log("service worker has been activated");
+    // deleting all older cache
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            // console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))   
+            )
+        })
+    )
 });
+
 // fetch event
 self.addEventListener('fetch', evt => {
-    console.log("fetch event", evt);
+    // console.log("fetch event", evt);
+    // checking for cached assets
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    );
 });
