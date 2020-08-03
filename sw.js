1f42c4fbdcd2ab8df7cdfd046d8aa575f45ceb98
edoc99 <harshitjainf3@gmail.com>
2020-08-03 11:40:29 +0530

    cache the dynamic pages

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const staticCacheName = 'site-static-v1';
+const dynamicCacheName = 'site-dynamic-v1';
 const assets = [
   '/',
   'index.html',
@@ -45,7 +46,12 @@ self.addEventListener('fetch', evt => {
     // checking for cached assets
     evt.respondWith(
         caches.match(evt.request).then(cacheRes => {
-            return cacheRes || fetch(evt.request);
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCacheName).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            });
         })
     );
 });
